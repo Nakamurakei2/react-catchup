6d7584eff3313ed747c4df00bf69953dd52ef966
Nakamurakei2 <ktyun2525@gmail.com>
2025-12-29 16:11:56 +0900

    feat: update React.memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React, { useCallback, useMemo, useState } from "react";
-
-// function Child({ value }: { value: number }) {
-//   console.log("child render");
-//   return <div>{value}</div>;
-// }
-
-const Child = React.memo(({ onClick }: { onClick: () => void }) => {
-  console.log("child render");
-  return <button onClick={onClick}>click</button>;
-});
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleClick = useCallback(() => {
-    console.log("clicked");
-  }, []);
+  const logCount = useCallback(() => {
+    console.log("logcount");
+    console.log(cp)
+  }, [count]);
 
   return (
     <div>
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-      <Child onClick={handleClick} />
+      <p>{count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>logcount</button>
     </div>
   );
 }
